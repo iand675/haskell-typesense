2eb2b441b9ed307c2cba924eaa779056c4c47261
Ian Duncan <ian@mercury.com>
2021-07-19 11:37:42 -0700

    Add compat note

--- src/Network/Typesense.hs
+++ src/Network/Typesense.hs
@@ -14,13 +14,21 @@ Maintainer  : ian@iankduncan.com
 Stability   : experimental
 Portability : POSIX
 
+= Introduction
+
 Typesense is an open-source, typo-tolerant search engine optimized for instant (typically sub-50ms) search-as-you-type experiences and developer productivity.
 
 If you've heard about ElasticSearch or Algolia, a good way to think about Typesense is that it is:
 
-An open source alternative to Algolia, with some key quirks solved and
-An easier-to-use batteries-included alternative to ElasticSearch
+1. An open source alternative to Algolia, with some key quirks solved
+2. An easier-to-use batteries-included alternative to ElasticSearch
+
 🗣️ 🎥 If you prefer watching videos, here's one where we introduce Typesense and show a walk-through: https://youtu.be/F4mB0x_B1AE?t=144
+
+=== Compatibility
+
+This version of the Haskell client library is currently tested against Typesense __v0.21__. Other versions may or may not work.
+
 -}
 module Network.Typesense
   (
